52f291b51a9d49df436af7eda0275aaddd18b87a
Justin Gifford <49419781+JGiffordX@users.noreply.github.com>
2023-07-11 15:03:38 -0400

    Menus, Meshes, and Mechanics
    - Added Main Menu
    - Added Settings Menu
    - Added Pause Menu (Q Key)
    - Added Inventory Menu (E Key)
    - Added the ability to grab and move objects (Hold Left Mouse Button)
    - Added the ability to (sort of) rotate objects while holding them (Hold Right Mouse Button)
    - Added a default HUD
    - Added several assets, including materials, static meshes, widgets, input actions, and levels
    - Changed ProjectGameModeBase to DefaultGameModeBase (to account for other game modes, like MenuGameModeBase)
    - Enabled Borderless Window mode
    - Disabled Lumen for Dynamic Global Illumination Method and Reflection Method for substantial FPS increase
    - Disabled support for all platforms except Windows and Mac

---
+++ Source/Project/AMenuGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AMenuGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API AMenuGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AMenuGameModeBase();
+};
--- Source/Project/Project.cpp
+++ Source/Project/Project.cpp
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+
 #include "Project.h"
 #include "Modules/ModuleManager.h"
 
